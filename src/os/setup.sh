cc1911a711b15c9724bb73ce656e6d8dfd922bbc
lucatronlk <alex.luka@hotmail.com>
2024-01-08 13:19:56 +0200

    fix

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -r GITHUB_REPOSITORY="Lucatronlk/dotfiles"
+declare -r GITHUB_REPOSITORY="Lucatronlk/dotfiles-ubuntu"
 
 declare -r DOTFILES_ORIGIN="git@github.com:$GITHUB_REPOSITORY.git"
 declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/main"
